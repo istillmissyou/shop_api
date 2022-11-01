e7ef33830b86e16dcb47dbdd38d27217ee4de72c
Данил Штунь <danilshtun@yandex.ru>
2022-11-01 11:26:36 +0500

    add models

--- guru/guru/settings.py
+++ guru/guru/settings.py
@@ -1,6 +1,10 @@
-from pathlib import Path
+import os
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
@@ -10,7 +14,7 @@ SECRET_KEY = 'django-insecure-0$ltiq1vg*&#1n1&(7@pp$=z8+v3@+j29lq(xo@#&bl)x9wx#7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -22,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'shops',
     'api',
 ]
@@ -38,10 +43,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'guru.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -58,18 +64,16 @@ WSGI_APPLICATION = 'guru.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -88,9 +92,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -100,11 +103,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- guru/shops/models.py
+++ guru/shops/models.py
@@ -1,3 +1,80 @@
-from django.db import models
+from django.db.models import (CASCADE, CharField, ForeignKey, Model,
+                              PositiveSmallIntegerField, TimeField)
 
-# Create your models here.
+
+class City(Model):
+    name = CharField(
+        unique=True,
+        max_length=200,
+        verbose_name='Название города',
+        help_text='Введите название города',
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+    def __str__(self):
+        return self.name
+
+
+class Street(Model):
+    name = CharField(
+        unique=True,
+        max_length=200,
+        verbose_name='Название улицы',
+        help_text='Введите название улицы',
+    )
+    city = ForeignKey(
+        City,
+        on_delete=CASCADE,
+        verbose_name='Город улицы',
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Улица'
+        verbose_name_plural = 'Улицы'
+
+    def __str__(self):
+        return self.name
+
+
+class Shop(Model):
+    name = CharField(
+        unique=True,
+        max_length=200,
+        verbose_name='Название магазина',
+        help_text='Введите название магазина',
+    )
+    city = ForeignKey(
+        City,
+        on_delete=CASCADE,
+        verbose_name='Город магазина',
+    )
+    street = ForeignKey(
+        Street,
+        on_delete=CASCADE,
+        verbose_name='Улица магазина',
+    )
+    house = PositiveSmallIntegerField(
+        verbose_name='Номер дома',
+        help_text='Введите номер дома',
+    )
+    open_time = TimeField(
+        verbose_name='Время открытия',
+        help_text='Введите время открытия',
+    )
+    close_time = TimeField(
+        verbose_name='Время закрытия',
+        help_text='Введите время закрытия',
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Магазин'
+        verbose_name_plural = 'Магазины'
+
+    def __str__(self):
+        return self.name
